90c18f65ea03c0fffb854fad769b915a479165b9
Guilherme Gutierres Gallo <guilherme.gg1@hotmail.com>
2018-11-30 15:50:26 -0200

    allow cors

--- config/custom-express.js
+++ config/custom-express.js
@@ -8,6 +8,13 @@ const logger = require('../services/logger');
 module.exports = function() {
   var app = express();
 
+  // Enable CORS
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use(morgan('common', {
     stream: {
       write: function(message) {
